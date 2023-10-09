dbb2c48fdf87cb8a532f66c68480c17329f34de6
baronovM <baronov05@gmail.com>
2023-10-09 11:19:22 +0300

    readability fix

--- contest1/G.cpp
+++ contest1/G.cpp
@@ -1,21 +1,19 @@
 #include <iostream>
 #include <vector>
 
-using Ll = long long;
-
 const int kBitInByte = 8;
 const int kByte = 256;
 const int kByteMask = 0b11111111;
 
-void LsdSort(std::vector<Ll>& arr) {
-  std::vector<std::vector<Ll>> bins(kByte);
-  for (int iter_num = 0; iter_num < int(sizeof(Ll)); ++iter_num) {
-    for (Ll x : arr) {
+void LSDSort(std::vector<long long>& arr) {
+  std::vector<std::vector<long long>> bins(kByte);
+  for (int iter_num = 0; iter_num < int(sizeof(long long)); ++iter_num) {
+    for (long long x : arr) {
       bins[(x >> (iter_num * kBitInByte)) & kByteMask].push_back(x);
     }
     int ind = 0;
     for (int j = 0; j < kByte; ++j) {
-      for (Ll x : bins[j]) {
+      for (long long x : bins[j]) {
         arr[ind++] = x;
       }
       bins[j].clear();
@@ -26,11 +24,11 @@ void LsdSort(std::vector<Ll>& arr) {
 int main() {
   int size;
   std::cin >> size;
-  std::vector<Ll> arr(size);
+  std::vector<long long> arr(size);
   for (int i = 0; i < size; ++i) {
     std::cin >> arr[i];
   }
-  LsdSort(arr);
+  LSDSort(arr);
   for (int i = 0; i < size; ++i) {
     std::cout << arr[i] << '\n';
   }
