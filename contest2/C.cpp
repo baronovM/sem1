4c67acaa6e1a83e83c98eee0d72e96d2062daf86
baronovM <baronov05@gmail.com>
2023-11-27 10:14:23 +0300

    Разделил логику на части и вынес из main

--- contest2/C.cpp
+++ contest2/C.cpp
@@ -1,32 +1,54 @@
 #include <deque>
 #include <iostream>
 
+class GoblinQueue {
+  std::deque<int> first_half_;
+  std::deque<int> second_half_;
+
+  void Balance() {
+    if (first_half_.size() < second_half_.size()) {
+      first_half_.push_back(second_half_.front());
+      second_half_.pop_front();
+    }
+    else if (first_half_.size() > second_half_.size() + 1) {
+      second_half_.push_front(first_half_.back());
+      first_half_.pop_back();
+    }
+  }
+  
+public:
+  void PushBack(int goblin) {
+    Balance();
+    second_half_.push_back(goblin);
+  }
+  void PushMid(int goblin) {
+    Balance();
+    first_half_.push_back(goblin);
+  }
+  int PopFront() {
+    Balance();
+    int res = first_half_.front();
+    first_half_.pop_front();
+    return res;
+  }
+};
+
 int main() {
   int num;
   std::cin >> num;
-  std::deque<int> first_half;
-  std::deque<int> second_half;
+  GoblinQueue que;
   char req;
   int goblin;
   while (num-- > 0) {
     std::cin >> req;
     if (req == '+') {
       std::cin >> goblin;
-      second_half.push_back(goblin);
+      que.PushBack(goblin);
     } else if (req == '*') {
       std::cin >> goblin;
-      first_half.push_back(goblin);
+      que.PushMid(goblin);
     } else if (req == '-') {
-      std::cout << first_half.front() << '\n';
-      first_half.pop_front();
-    }
-
-    if (first_half.size() < second_half.size()) {
-      first_half.push_back(second_half.front());
-      second_half.pop_front();
-    } else if (first_half.size() > second_half.size() + 1) {
-      second_half.push_front(first_half.back());
-      first_half.pop_back();
+      std::cout << que.PopFront() << '\n';
     }
   }
 
